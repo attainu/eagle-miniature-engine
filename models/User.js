9e4a208c51ac45a6abd79df32a5e837cd61c8eb4
Nishant Ghodke <nishant@attainu.com>
2019-11-13 23:08:16 +0530

    Added a sample method in User model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     username: String,
     facebookId: String
 });
 
-const User = mongoose.model('user',userSchema);
+UserSchema.statics.someMethod = function(userId, cb) {
+  User.findById(userId, function(error, user) {
+    if(error) {
+      return cb(error);
+    }
+
+    return cb(null, user);
+  });
+}
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
