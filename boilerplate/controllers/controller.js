fcfc3b5c5cb393c3ae21e6b522c72ec5d47de516
sumanth-kasi-vst-au4 <54485997+sumanth-kasi-vst-au4@users.noreply.github.com>
2019-11-14 16:45:17 +0530

    BAsic
    SChematics of  controller and model

---
+++ boilerplate/controllers/controller.js
@@ -0,0 +1,44 @@
+Control={};
+
+const Model=require('../models/goeagle.js');
+
+Control.home=function(req,res){
+    if(error){
+        return res.render({error:'Error,please reload or wait for sometime while the page reloads.Thank you!'});
+    }
+    return res.render('home');
+}
+
+Control.appTray=function(req,res){
+    if(error){
+        return res.render({error:'Error,please reload or wait for sometime while the page reloads.Thank you!'});
+    }
+    return res.render('appTray');
+}
+
+Control.Quiz=function(req,res){
+ Model.quiz(function(error,data){
+     if(error){
+         return res.render({error:'Error,please reload or wait for sometime while the page reloads.Thank you!'});
+     }
+     return res.render('Quiz',{});
+ });
+}
+
+
+Control.Quotes=function(req,res){
+    Model.quotes(function(error,data){
+        if(error){
+            return res.render({error:'Error,please reload or wait for sometime while the page reloads.Thank you!'});
+        }
+        return res.render('Quotes',{});
+    });
+
+}  
+
+
+
+module.exports=Control;
+
+
+
