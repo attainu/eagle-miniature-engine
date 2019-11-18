5ada0eaddb753754d16294e931d14ed861523551
Pavan Kumar Kallati <kallatipavankumar@gmail.com>
2019-11-18 07:26:03 +0530

    Fetch more data from fb and add to db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,13 +3,13 @@ const FacebookStrategy = require('passport-facebook');
 const keys = require('./keys');
 const User = require('./../models/User');
 
-passport.serializeUser((user,done)=>{
-    done(null,user.id);
+passport.serializeUser((user, done) => {
+    done(null, user.id);
 });
 
-passport.deserializeUser((id,done)=>{
-    User.findById(id).then((user)=>{
-        done(null,user);
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
     });
 });
 
@@ -19,26 +19,37 @@ passport.use(new FacebookStrategy({
     // strategy options
     clientID: keys.facebook.appID,
     clientSecret: keys.facebook.appSecret,
-    callbackURL:'/facebook/redirect'
+    callbackURL: '/facebook/redirect'
 
-    },function(accessToken, refreshToken, profile, done){
-    //passport callback
-
-    User.findOne({facebookId: profile.id}).then(function(currentUser){
-        if(currentUser){
-            console.log('User is ', currentUser);
-            done(null, currentUser);
-        }
-        else
-        {
-            new User({
-                username   : profile.displayName,
-                facebookId : profile.id
-            }).save().then(function(newUser){
-                console.log("new user created "+ newUser);
-                done(null, newUser);
-            });
-        }
-    });
+    profileFields: ['id', 'displayName', 'name', 'photos', 'email', 'gender', 'link'],
+    proxy: true
+},
+    (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+        User.findOne({ facebook: profile.id })
+            .then((user) => {
+                if (user) {
+                    done(null, user);
+                } else {
+                    const newUser = {
+                        facebook: profile.id,
+                        fullname: profile.displayName,
+                        firstname: profile.name.givenName,
+                        lastname: profile.name.familyName,
+                        image: `https://graph.facebook.com/${profile.id}/picture?type=large`
+                    }
+                    console.log(newUser);
+                    new User(newUser).save()
+                        .then((user) => {
+                            done(null, user);
+                        })
+                }
+            }).catch((err) => {
+                if (err) {
+                    throw err;
+                }
+            })
+    }
+));
 
 }));
--- models/User.js
+++ models/User.js
@@ -2,19 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: String,
-    facebookId: String
+  fullname: {
+    type: String,
+    default: ''
+  },
+  firstname: {
+    type: String,
+    default: ''
+  },
+  lastname: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  fbTokens: Array,
+  facebook: {
+    type: String
+  },
 });
 
-UserSchema.statics.someMethod = function(userId, cb) {
-  User.findById(userId, function(error, user) {
-    if(error) {
-      return cb(error);
-    }
+// UserSchema.statics.someMethod = function (userId, cb) {
+//   User.findById(userId, function (error, user) {
+//     if (error) {
+//       return cb(error);
+//     }
 
-    return cb(null, user);
-  });
-}
+//     return cb(null, user);
+//   });
+// }
 
 const User = mongoose.model('User', UserSchema);
 
