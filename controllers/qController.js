8e2750490019a9519c77c33bfec12eeef859bcbb
Pavan Kumar Kallati <kallatipavankumar@gmail.com>
2019-11-21 15:07:10 +0530

    Removed console.logs

--- controllers/qController.js
+++ controllers/qController.js
@@ -4,11 +4,11 @@ var counter = 0;
 var qstncount = 0;
 var x;
 var y;
-Control.quizData = function(req, res){
-     //Showing the Question and Answer on the Page
-        model.Qstndata(function(err, data){
+Control.quizData = function (req, res) {
+        //Showing the Question and Answer on the Page
+        model.Qstndata(function (err, data) {
                 res.render('pubgQuiz', {
-                        question:data.question,
+                        question: data.question,
                         option1: data.option[0],
                         option2: data.option[1],
                         option3: data.option[2],
@@ -17,37 +17,33 @@ Control.quizData = function(req, res){
         });
         // Taking the value of the button clicked by the user
         x = req.body.opt;
-        console.log(x);
         // Storing the answer in a variable
-        model.Answerdata(function(err, ansdata){
+        model.Answerdata(function (err, ansdata) {
                 y = ansdata.answer;
-                console.log(y, "answer");
-                if(x===y){
-                        console.log("true", x,"similar to", y);
+                if (x === y) {
                         counter++;
                         qstncount++;
-                        console.log(counter,"", qstncount);
                 }
-                else{
+                else {
                         counter = counter;
                         qstncount++;
-                        console.log(counter, "", qstncount);
                 }
-                console.log(counter,"", qstncount);
-                        
-        })
-        if(qstncount >20){
-                model.result(counter,function(error,data){
-                    if(error){ return res.send('Error!'); }
-                    
-                return res.render('result',{
-                    result:counter*5,
-                    image:data
 
-                });
+        })
+        if (qstncount > 20) {
+                model.result(counter, function (error, data) {
+                        if (error) {
+                                return res.send('Error!');
+                        }
+
+                        return res.render('result', {
+                                result: counter * 5,
+                                image: data
+
+                        });
                 });
         }
-        
+
 }
 
 module.exports = Control;
--- models/quizModel.js
+++ models/quizModel.js
@@ -3,53 +3,52 @@ const pubgQuiz = require('./../data/quiz.json');
 const pubgAnswer = require('./../data/quizanswers.json');
 var i = 0;
 var j = 0;
-PUBG.Qstndata = function(cb){
-        
-       while(i<pubgQuiz.length){
-               i++;
-        return cb(null, pubgQuiz[i-1]);
-       }
-}
-PUBG.Answerdata = function(cb){
-        
-       while(j<pubgAnswer.length){
-               j++;
-        return cb(null, pubgAnswer[j-1]);
-       }
-}
- PUBG.result = function(rank, cb){
-         console.log(rank);
-                if(rank >=19 ){
-                        imgSrc = '/static/images/pubgConqueror.jpg';
-                        
-                }else if(rank ===18){
-                        imgSrc = '/static/images/pubgAce.jpg';
-                        
-                }else if(rank ===16 ){
-                        imgSrc = '/static/images/pubgCrown.jpg';
-                        
-                }
-                else if(rank ===14 ){
-                        imgSrc = 'static/images/pubgDiamond.jpg';
-                        
-                }
-                else if( rank ===12 ){
-                        imgSrc = '/static/images/pubgPlatinum.jpg';
-                        
-                }
-                else if(rank ===10 ){
-                        imgSrc = '/static/images/pubgGold.jpg';
-                        
-                }else if(rank ===8 ){
-                        imgSrc = '/static/images/pubgSilver.jpg';
-                        
-                }
-                else {
-                        imgSrc = '/static/images/pubgBronze.jpg';
-                        
-                }
+PUBG.Qstndata = function (cb) {
 
-        return cb(null,imgSrc);
-   
+        while (i < pubgQuiz.length) {
+                i++;
+                return cb(null, pubgQuiz[i - 1]);
+        }
+}
+PUBG.Answerdata = function (cb) {
+
+        while (j < pubgAnswer.length) {
+                j++;
+                return cb(null, pubgAnswer[j - 1]);
+        }
+}
+PUBG.result = function (rank, cb) {
+        if (rank >= 19) {
+                imgSrc = '/static/images/pubgConqueror.jpg';
+
+        } else if (rank === 18) {
+                imgSrc = '/static/images/pubgAce.jpg';
+
+        } else if (rank === 16) {
+                imgSrc = '/static/images/pubgCrown.jpg';
+
+        }
+        else if (rank === 14) {
+                imgSrc = 'static/images/pubgDiamond.jpg';
+
+        }
+        else if (rank === 12) {
+                imgSrc = '/static/images/pubgPlatinum.jpg';
+
+        }
+        else if (rank === 10) {
+                imgSrc = '/static/images/pubgGold.jpg';
+
+        } else if (rank === 8) {
+                imgSrc = '/static/images/pubgSilver.jpg';
+
+        }
+        else {
+                imgSrc = '/static/images/pubgBronze.jpg';
+
+        }
+
+        return cb(null, imgSrc);
+
 }
 module.exports = PUBG;
