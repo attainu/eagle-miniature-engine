61e34aa5a8ff52958ba9beb454255814d0e27742
Nitish <nitishh21@yahoo.com>
2019-11-21 21:34:21 +0530

    Updated auth on homepage

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-router.get('/', passport.authenticate('facebook'));
+router.get('/', function(req, res){
+    res.render('homepage');
+});
+
+router.get('/login', passport.authenticate('facebook'));
 
 router.get('/facebook/redirect', passport.authenticate('facebook'),function (req, res) {
     res.redirect('/apps');
