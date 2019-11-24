26ba1278582fb8094f63d2c34d87e6a4f5dd6bfe
Pavan Kumar Kallati <kallatipavankumar@gmail.com>
2019-11-24 17:51:35 +0530

    UI for name Adjective app

--- controllers/nameapp.js
+++ controllers/nameapp.js
@@ -3,26 +3,31 @@ const nameData = require('../data/nameApp.json');
 var array = [];
 var data = [];
 
-controller.retrieve = function(req, res){
+controller.retrieve = function (req, res) {
     var name = req.user.firstname;
     var nameArray = name.split('');
-    for (var i=0;i<nameArray.length;i++){
-        for(var j=0;j<nameData.length;j++){
-            if(nameArray[i].toLowerCase() === nameData[j].word.charAt(0).toLowerCase()){
+    for (var i = 0; i < nameArray.length; i++) {
+        for (var j = 0; j < nameData.length; j++) {
+            if (nameArray[i].toLowerCase() === nameData[j].word.charAt(0).toLowerCase()) {
                 data.push(nameData[j].word);
             }
         }
-        
-        var random = Math.floor(Math.random()*data.length);
+
+        var random = Math.floor(Math.random() * data.length);
         array.push(data[random]);
         data = [];
     }
-   
-    res.render('name',{
-        name:nameArray,
-        result:array
+    var out = [];
+    for (var i = 0; i < name.length; i++) {
+        var output = new Object();
+        output.char = name[i];
+        output.adj = array[i];
+        out.push(output);
+    }
+    res.render('name', {
+        output: out,
+        name: name
     });
-    array = [];
 }
 
 module.exports = controller; 
