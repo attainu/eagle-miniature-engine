8e6463b63d652fe60fc66e0283cb23628abb71f0
sumanth-kasi-vst-au4 <54485997+sumanth-kasi-vst-au4@users.noreply.github.com>
2019-11-26 03:30:56 +0530

    0330 26/11/2019

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
+   
     $('#new_name').on('click', function () {
         console.log('clicked!');
         $('#form').removeClass("d-none");
+    });
+    $('#change').on('click', function () {
+               $.ajax({
+            method:"GET",
+            url:"../../../data/quotes.json",
+            
+            success:function(quotes){
+             var i = Math.floor(Math.random() * 109);
+              $('#quote').html(quotes[i].quote);
+             
+            },
+            error:function(error){
+                alert('Fetching failed',error);
+            }
+        });
     });
 });
