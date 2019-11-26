a30b1d3137ccdb42c895582bda3b0efff34fa5c8
Nitish <nitishh21@yahoo.com>
2019-11-26 15:02:07 +0530

    Fixed quotes ajax url

--- public/JS/script.js
+++ public/JS/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $('#change').on('click', function () {
                $.ajax({
             method:"GET",
-            url:"http://entertaining--apps.herokuapp.com/static/data/quotes.json",
+            url:"/static/data/quotes.json",
             
             success:function(quotes){
              var i = Math.floor(Math.random() * 109);
