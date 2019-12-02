e415418ee725d07a2aa910e08cb7fa1eb2d88d07
Pavan Kumar Kallati <kallatipavankumar@gmail.com>
2019-12-02 23:17:39 +0530

    Fixing Biggest mistake app share button

--- public/JS/mistakeApp.js
+++ public/JS/mistakeApp.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
           uniqueURL: uniqueURL
         }
       }).done(function (o) {
-        var frame = '<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fentertaining--apps.herokuapp.com%2Fapps%2Fname%2F' + uniqueURL + '%23&layout=button&size=large&appId=473078063552105&width=77&height=28" width="77" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>'
+        var frame = '<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fentertaining--apps.herokuapp.com%2Fapps%2FbiggestMistake%2F' + uniqueURL + '%23&layout=button&size=large&appId=473078063552105&width=77&height=28" width="77" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>'
         $('.fb-share').html(frame);
         $('.first').removeClass('d-none');
         $("#share-btn").removeClass("d-none");
