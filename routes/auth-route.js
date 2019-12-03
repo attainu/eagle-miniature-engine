9107e5381a2a2df2f51eae4de30b0bc180dcc41d
Nitish <nitishh21@yahoo.com>
2019-12-03 10:59:43 +0530

    Fixed Name app UI

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -2,7 +2,14 @@ const router = require('express').Router();
 const passport = require('passport');
 
 router.get('/', function(req, res){
-    res.render('appTray');
+    res.locals.metaTags = {
+        title: "Cheap Thrills ",
+        description: "Entertaining games created using FB Graph API",
+        url: "https://entertaining--apps.herokuapp.com"
+    };
+    res.render('appTray',{
+        img: 'http://entertaining--apps.herokuapp.com/static/images/I_love_cheap_thrills.png'
+    });
 });
 
 router.get('/login', passport.authenticate('facebook'));
